d8c43cb94d264d3354226eb3234554ea68c359ac
ermam <ermammoser@gmail.com>
2015-10-21 13:13:45 -0400

    Fixed unspecified types for the TimeObjects comparators

--- Software-Project-Day/src/TimeObject.java
+++ Software-Project-Day/src/TimeObject.java
@@ -1,78 +1,45 @@
 import java.util.Comparator;
 
-public class TimeObject implements Comparator, Comparable {
+public class TimeObject implements Comparator<TimeObject>, Comparable<TimeObject> {
 	public int hour, minute;
-	
-	public TimeObject(int hour, int minute)
-	{
+
+	public TimeObject(int hour, int minute) {
 		this.hour = hour;
 		this.minute = minute;
 	}
-	
 
 	@Override
-	public int compareTo(Object arg0) {
-		if(arg0 instanceof TimeObject)
-		{
-			TimeObject compare = (TimeObject)arg0;
-			if(this.hour > compare.hour)
-			{
+	public int compareTo(TimeObject compare) {
+		if (this.hour > compare.hour) {
+			return 1;
+		} else if (this.hour < compare.hour) {
+			return -1;
+		} else {
+			if (this.minute > compare.minute) {
 				return 1;
-			}
-			else if(this.hour < compare.hour)
-			{
+			} else if (this.minute < compare.minute) {
 				return -1;
-			}
-			else
-			{
-				if(this.minute > compare.minute)
-				{
-					return 1;
-				}
-				else if(this.minute < compare.minute)
-				{
-					return -1;
-				}
-				else
-				{
-					return 0;
-				}
+			} else {
+				return 0;
 			}
 		}
-		return 0;
 	}
 
 	@Override
-	public int compare(Object arg0, Object arg1) {
-		if(arg0 instanceof TimeObject && arg1 instanceof TimeObject)
-		{
-			TimeObject compare = (TimeObject)arg0;
-			TimeObject compare2 = (TimeObject)arg1;
-			if(compare.hour > compare2.hour)
-			{
+	public int compare(TimeObject compare, TimeObject compare2) {
+		if (compare.hour > compare2.hour) {
+			return 1;
+		} else if (compare.hour < compare2.hour) {
+			return -1;
+		} else {
+			if (compare.minute > compare2.minute) {
 				return 1;
-			}
-			else if(compare.hour < compare2.hour)
-			{
+			} else if (compare.minute < compare2.minute) {
 				return -1;
-			}
-			else
-			{
-				if(compare.minute > compare2.minute)
-				{
-					return 1;
-				}
-				else if(compare.minute < compare2.minute)
-				{
-					return -1;
-				}
-				else
-				{
-					return 0;
-				}
+			} else {
+				return 0;
 			}
 		}
-		return 0;
 	}
-	
+
 }
