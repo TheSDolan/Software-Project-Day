262e1ae0da71e26a5124aee6a28b3f17e6cee8c2
ermam <ermammoser@gmail.com>
2015-10-23 13:42:17 -0400

    Finished implementation of the manager

--- Software-Project-Day/src/Manager.java
+++ Software-Project-Day/src/Manager.java
@@ -4,8 +4,11 @@ public class Manager extends Thread {
 	private ManagerRoom managerRoom;
 	private ConferenceRoom conference;
 
+	// Important times
 	private static final TimeObject SHOW_UP_TIME = new TimeObject(8, 0);
 	private static final TimeObject GO_HOME_TIME = new TimeObject(17, 0);
+	private static final TimeObject MEETING_ONE = new TimeObject(10, 0);
+	private static final TimeObject MEETING_TWO = new TimeObject(14, 0);
 
 	public Manager(ManagerRoom managerRoom, ConferenceRoom conference) {
 		this.managerRoom = managerRoom;
@@ -25,17 +28,34 @@ public class Manager extends Thread {
 			// join the morning meeting
 			managerRoom.managerJoinManagerMeeting();
 			
+			while(Time.getTime().compareTo(MEETING_ONE)< 0){
+				doNormalTask();
+			}
+			
+			// Go to first executive meeting
+			InstantPrint.PrintInstantly(Time.getTime() + " " + Thread.currentThread().getName() + " goes to executive meeting.");
+			Thread.sleep(Time.getPause(new TimeObject(1,0)));
+			InstantPrint.PrintInstantly(Time.getTime() + " " + Thread.currentThread().getName() + " returns from executive meeting.");
 			
 			//do work, potentially generate questions until lunch time
 			while(Time.getTime().compareTo(Time.LUNCH_TIME) < 0){
-
+				doNormalTask();
 			}
+			// Go to lunch
 			InstantPrint.PrintInstantly(Time.getTime() + " " + Thread.currentThread().getName() + " goes to lunch.");
 			Thread.sleep(Time.getPause(new TimeObject(1,0)));
 			InstantPrint.PrintInstantly(Time.getTime() + " " + Thread.currentThread().getName() + " returns from lunch.");
 
+			while(Time.getTime().compareTo(MEETING_TWO)< 0){
+				doNormalTask();
+			}
+			// Go to second executive meeting
+			InstantPrint.PrintInstantly(Time.getTime() + " " + Thread.currentThread().getName() + " goes to executive meeting.");
+			Thread.sleep(Time.getPause(new TimeObject(1,0)));
+			InstantPrint.PrintInstantly(Time.getTime() + " " + Thread.currentThread().getName() + " returns from executive meeting.");
+			
 			while(Time.getTime().compareTo(Time.MEETING_TIME)< 0){
-				
+				doNormalTask();
 			}
 			
 			conference.managerJoinEndOfDayMeeting();
@@ -49,5 +69,11 @@ public class Manager extends Thread {
 		} catch (InterruptedException e) {
 
 		}
+	}
+	
+	private void doNormalTask() throws InterruptedException
+	{
+		managerRoom.checkQuestion();
+		Thread.sleep(Time.getPause(new TimeObject(0,1)));
 	}
 }
